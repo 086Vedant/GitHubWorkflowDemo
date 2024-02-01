fa43941f4d548d7b8c0a3b9130d62d3b3d1b2751
086Vedant <vedant261017@gmail.com>
2024-02-01 13:15:03 +0530

    modify java app

--- java_app/CalculatorTest.java
+++ java_app/CalculatorTest.java
@@ -1,17 +1,12 @@
 // CalculatorTest.java
-import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
+    
     @Test
     public void testAdd() {
         Calculator calculator = new Calculator();
         assertEquals(5, calculator.add(2, 3));
-    }
-
-    @Test
-    public void testSubtract() {
-        Calculator calculator = new Calculator();
-        assertEquals(2, calculator.subtract(5, 3));
     }
 }
