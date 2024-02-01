28985782ac2984de0de4f92d7bcdbe2343283744
086Vedant <89913569+086Vedant@users.noreply.github.com>
2024-02-01 14:13:58 +0530

    Update CalculatorTest.java

--- src/test/resource/CalculatorTest.java
+++ src/test/resource/CalculatorTest.java
@@ -4,9 +4,9 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class CalculatorTest {
     
-    @Test
-    public void testAdd() {
-        Calculator calculator = new Calculator();
-        assertEquals(5, calculator.add(2, 3));
-    }
+    // @Test
+    // public void testAdd() {
+    //     Calculator calculator = new Calculator();
+    //     assertEquals(5, calculator.add(2, 3));
+    // }
 }
